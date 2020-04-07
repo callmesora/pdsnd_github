5f774d5aff7b364f2575b53c23e74acfdced41db
Pedro <pedroazevedo6@gmail.com>
2020-04-07 19:42:32 +0100

    Refracturing. Added Discription to main()
    Further explained what main fuction is doing with added doc

--- bikeshare.py
+++ bikeshare.py
@@ -212,12 +212,14 @@ def main():
     while True:
         city, month, day = get_filters()
         df = load_data(city, month, day)
-
+        
+        #print all the stats
         time_stats(df)
         station_stats(df)
         trip_duration_stats(df)
         user_stats(df,city)
         
+        #display Raw Data
         display_data(df)
 
         restart = input('\nWould you like to restart? Enter yes or no.\n')
